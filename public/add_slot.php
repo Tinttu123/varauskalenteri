237975cb0d86afecd2e17da54b618059e75d28a9
Tiina Soramies <p33576@edu.sasky.fi>
2025-11-29 08:24:55 +0200

    Ensimmäinen commit githubiin

--- public/add_slot.php
+++ public/add_slot.php
@@ -41,10 +41,10 @@ $allSlots = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 <html lang="fi">
 <head>
   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
   <title>Lisää aika</title>
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/calendar.css">
+  <!--link rel="stylesheet" href="css/calendar.css"-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
     .add-slot, .slot-table {
